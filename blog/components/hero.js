fc50b6aa4091370160948d9cb1db7aa9cf3b83fc
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-08 07:48:32 +0900

    add: ヒーローの画像を表示

--- blog/components/hero.js
+++ blog/components/hero.js
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import React from "react";
 import styles from "styles/hero.module.css";
+import cube from "images/cube.jpg";
 
 const Hero = ({ title, subtitle, imageOn = false }) => {
   return (
@@ -7,8 +9,19 @@ const Hero = ({ title, subtitle, imageOn = false }) => {
       <div className={styles.text}>
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.subtitle}>{subtitle}</p>
-        {imageOn && <figure>[画像]</figure>}
       </div>
+      {imageOn && (
+        <figure className={styles.image}>
+          <Image
+            src={cube}
+            alt=""
+            layout="responsive"
+            sizes="(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw"
+            priority
+            placeholder="blur"
+          />
+        </figure>
+      )}
     </div>
   );
 };
