567272132e339c6fa021894f7b4a1f09135743a2
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-07 17:47:08 +0900

    add: トップページの構成要素

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -3,5 +3,14 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <h1>CUBE</h1>;
+  return (
+    <>
+      <header>HEADER</header>
+      <main>
+        <h1>CUBE</h1>
+        <div>アウトプットしていくサイト</div>
+      </main>
+      <footer>FOOTER</footer>
+    </>
+  );
 }
