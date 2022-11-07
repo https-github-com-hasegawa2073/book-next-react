e7842f5c25d157f42bdaf584023afc65f27804a6
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-07 23:37:52 +0900

    add: ヘッダーのレイアウト

--- blog/components/header.js
+++ blog/components/header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Logo from "./logo";
 import Nav from "./nav";
+import styles from "styles/header.module.css";
 
 function Header() {
   return (
     <header>
-      <Logo boxOn />
-      <Nav />
+      <div className={styles.flexContainer}>
+        <Logo boxOn />
+        <Nav />
+      </div>
     </header>
   );
 }
