ba9adaa01176f779ff93d02a0cbd507e3468ab4e
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-07 21:19:13 +0900

    fix: 絶対パスでインポートできるようにした

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,9 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import Footer from "../components/footer";
-import Header from "../components/header";
-import Hero from "../components/hero";
-import styles from "../styles/Home.module.css";
+import Footer from "components/footer";
+import Header from "components/header";
+import Hero from "components/hero";
 
 export default function Home() {
   return (
