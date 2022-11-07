27f1aa63c3386f2dbb394cf032f4737e3cd89c0a
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-08 08:05:21 +0900

    chore: fontawesomeのアイコンを使えるようにした

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,6 +1,11 @@
 import Layout from "components/layout";
 import "../styles/globals.css";
 
+// Font Awesomeの設定
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
