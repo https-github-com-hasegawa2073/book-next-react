5262aaf6855b819e7231d3ca59a10cf92c4ba35e
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-07 23:07:58 +0900

    add: ヒーローのテキストスタイルを設定

--- blog/components/hero.js
+++ blog/components/hero.js
@@ -4,8 +4,8 @@ import styles from "styles/hero.module.css";
 const Hero = ({ title, subtitle, imageOn = false }) => {
   return (
     <div className={styles.text}>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.subtitle}>{subtitle}</p>
       {imageOn && <figure>[画像]</figure>}
     </div>
   );
