7a96db5ac7bb7bab1a273303bc4011a5c501c33b
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-09 12:30:23 +0900

    add: ページタイトルを共有

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -3,10 +3,12 @@ import Footer from "components/footer";
 import Header from "components/header";
 import Hero from "components/hero";
 import Layout from "components/layout";
+import Meta from "components/meta";
 
 export default function Home() {
   return (
     <Container>
+      <Meta />
       <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
     </Container>
   );
